025c396e6c01c552f0c89458d0b1cd7f7a720a38
ImDav1dReal <115368785+ImDav1dReal@users.noreply.github.com>
2022-12-30 12:41:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
 
-showTime();
+document.title = "ImDav1d Packages - Downloads";
+
+const themeToggleButton = document.getElementById("theme-toggle");
+
+
+themeToggleButton.addEventListener("click", function() {
+
+  if (document.body.classList.contains("light-theme")) {
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+    document.body.classList.add("light-theme");
+    function switchTheme() {
+    const h4 = document.querySelector('h4')
+    h4.classList.toggle('dark-theme');
+    h4.classList.toggle('light-theme');
+}
+  }
+});
+
+
